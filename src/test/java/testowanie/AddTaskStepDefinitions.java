c118d3775a5760c8b5341fbdd80b14b4fa0e90ae
Karol Zalewski <zalewski.karol@op.pl>
2024-01-10 17:50:04 +0100

    Dodano testy przeciazeniowe do metody dodawania zadan

--- src/test/java/testowanie/AddTaskStepDefinitions.java
+++ src/test/java/testowanie/AddTaskStepDefinitions.java
@@ -44,6 +44,40 @@ public class AddTaskStepDefinitions {
             assertEquals(Priority.LOW, addedTask.getPriority());
     }
 
+    // 1p
+
+    @Given("Użytkownik uruchamia aplikację_1p")
+    public void użytkownik_uruchamia_aplikację_1p() {
+        taskController = new TaskController();
+    }
+    @When("Wprowadza długą nazwę zadania")
+    public void wprowadza_długą_nazwę_zadania() {
+        StringBuilder longTaskName = new StringBuilder();
+        for (int i = 0; i < 50000000; i++)
+            longTaskName.append("zadanie");
+        taskName = longTaskName.toString();
+        taskDetails = "";
+        assignedUser = "";
+        assignedPriority = Priority.LOW; 
+    }
+    @And("Wciska przycisk Add_1p")
+    public void wciska_przycisk_add_1p() {
+        taskController.addTaskDirectly(new Task(taskName, taskDetails, assignedUser, assignedPriority));
+    }
+    @Then("Nowe zadanie z długą nazwą zostaje dodane na listę zadań")
+    public void nowe_zadanie_z_długą_nazwą_zostaje_dodane_na_listę_zadań() {
+            List<Task> tasks = taskController.getTasks();
+            Task addedTask = tasks.get(0);
+            StringBuilder longTaskName = new StringBuilder();
+            for (int i = 0; i < 50000000; i++)
+                longTaskName.append("zadanie");
+            assertEquals(1, tasks.size());
+            assertEquals(longTaskName.toString(), addedTask.getName());
+            assertEquals("", addedTask.getDetails());
+            assertEquals("", addedTask.getUser());
+            assertEquals(Priority.LOW, addedTask.getPriority());
+    }
+
     // 2
 
     @Given("Użytkownik uruchamia aplikację_2")
@@ -71,6 +105,40 @@ public class AddTaskStepDefinitions {
             assertEquals("", addedTask.getUser());
             assertEquals(Priority.LOW, addedTask.getPriority());
     }
+
+    // 2p
+
+    @Given("Użytkownik uruchamia aplikację_2p")
+    public void użytkownik_uruchamia_aplikację_2p() {
+        taskController = new TaskController();
+    }
+    @When("Wprowadza długą nazwę zadania oraz długie szczegóły")
+    public void wprowadza_długą_nazwę_zadania_oraz_długie_szczegóły() {
+        StringBuilder longTaskName = new StringBuilder();
+        for (int i = 0; i < 50000000; i++)
+            longTaskName.append("zadanie");
+        taskName = longTaskName.toString();
+        taskDetails = longTaskName.toString();
+        assignedUser = "";
+        assignedPriority = Priority.LOW; 
+    }
+    @And("Wciska przycisk Add_2p")
+    public void wciska_przycisk_add_2p() {
+        taskController.addTaskDirectly(new Task(taskName, taskDetails, assignedUser, assignedPriority));
+    }
+    @Then("Nowe zadanie z długą nazwą oraz długimi szczegółami zostaje dodane na listę zadań")
+    public void nowe_zadanie_z_długą_nazwą_oraz_długimi_szczegółami_zostaje_dodane_na_listę_zadań() {
+            List<Task> tasks = taskController.getTasks();
+            Task addedTask = tasks.get(0);
+            StringBuilder longTaskName = new StringBuilder();
+            for (int i = 0; i < 50000000; i++)
+                longTaskName.append("zadanie");
+            assertEquals(1, tasks.size());
+            assertEquals(longTaskName.toString(), addedTask.getName());
+            assertEquals(longTaskName.toString(), addedTask.getDetails());
+            assertEquals("", addedTask.getUser());
+            assertEquals(Priority.LOW, addedTask.getPriority());
+    } 
 
     // 3
 
@@ -182,6 +250,70 @@ public class AddTaskStepDefinitions {
             assertEquals("Szczegóły zadania 6", addedTask.getDetails());
             assertEquals("Nowy User 6", addedTask.getUser());
             assertEquals(Priority.HIGH, addedTask.getPriority());
+    }
+
+    // 6p
+
+    @Given("Użytkownik uruchamia aplikację_6p")
+    public void użytkownik_uruchamia_aplikację_6p() {
+        taskController = new TaskController();
+    }
+    @When("Wprowadza długą nazwę zadania, długie szczegóły, długiego użytkownika oraz priorytet")
+    public void wprowadza_długą_nazwę_zadania_długie_szczegóły_długiego_użytkownika_oraz_priorytet() {
+        StringBuilder longName = new StringBuilder();
+        for (int i = 0; i < 50000000; i++)
+            longName.append("zadanie");
+        taskName = longName.toString();
+        taskDetails = longName.toString();
+        assignedUser = longName.toString();
+        assignedPriority = Priority.HIGH; 
+    }
+    @And("Wciska przycisk Add_6p")
+    public void wciska_przycisk_add_6p() {
+        taskController.addTaskDirectly(new Task(taskName, taskDetails, assignedUser, assignedPriority));
+    }
+    @Then("Nowe zadanie z długą nazwą, długimi szczegółami, długim użytkownikiem oraz priorytetem zostaje dodane na listę zadań")
+    public void nowe_zadanie_z_długą_nazwą_długimi_szczegółami_długim_przypisanym_użytkownikiem_oraz_priorytetem_zostaje_dodane_na_listę_zadań() {
+        List<Task> tasks = taskController.getTasks();
+            Task addedTask = tasks.get(0);
+            StringBuilder longName = new StringBuilder();
+            for (int i = 0; i < 50000000; i++)
+                longName.append("zadanie");
+            assertEquals(1, tasks.size());
+            assertEquals(longName.toString(), addedTask.getName());
+            assertEquals(longName.toString(), addedTask.getDetails());
+            assertEquals(longName.toString(), addedTask.getUser());
+            assertEquals(Priority.HIGH, addedTask.getPriority());
+    }
+
+    // 7
+
+    @Given("Użytkownik uruchamia aplikację_7")
+    public void użytkownik_uruchamia_aplikację() {
+        taskController = new TaskController();
+    }
+    @When("Wprowadza dane zadań i wciska przycisk Add 10000000 razy")
+    public void wprowadza_dane_zadań_i_wciska_przycisk_Add_10000000_razy() {
+        for (int i = 1; i <= 10000000; i++) {
+            taskName = "Nowe Zadanie " + i;
+            taskDetails = "Szczegóły Zadania " + i;
+            assignedUser = "Nowy User " + i;
+            assignedPriority = Priority.MEDIUM;
+    
+            taskController.addTaskDirectly(new Task(taskName, taskDetails, assignedUser, assignedPriority));
+        }
+    }
+    @Then("Wszystkie 10000000 zadań zostaje poprawnie dodane na listę zadań")
+    public void wszystkie_10000000_zadań_zostaje_poprawnie_dodane_na_listę_zadań() {
+        List<Task> tasks = taskController.getTasks();
+        assertEquals(10000000, tasks.size());
+        for (int i = 0; i < 10000000; i++) {
+            Task addedTask = tasks.get(i);
+            assertEquals("Nowe Zadanie " + (i + 1), addedTask.getName());
+            assertEquals("Szczegóły Zadania " + (i + 1), addedTask.getDetails());
+            assertEquals("Nowy User " + (i + 1), addedTask.getUser());
+            assertEquals(Priority.MEDIUM, addedTask.getPriority());
+        }
     }
 
 }
